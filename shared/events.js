6f9ef79cc5ab7f6b2c9da6a7033b20140793927e
Proppls <99088284+Proppls@users.noreply.github.com>
2026-06-14 12:21:13 +0530

    Add SOCKET_EVENTS for socket communication

---
+++ shared/events.js
@@ -0,0 +1,18 @@
+export const SOCKET_EVENTS = {
+  CREATE_ROOM: 'create-room',
+  ROOM_CREATED: 'room-created',
+
+  JOIN_ROOM: 'join-room',
+  ROOM_JOINED: 'room-joined',
+
+  PEER_JOINED: 'peer-joined',
+  PEER_DISCONNECTED: 'peer-disconnected',
+
+  OFFER: 'offer',
+  ANSWER: 'answer',
+  ICE_CANDIDATE: 'ice-candidate',
+
+  ROOM_ERROR: 'room-error',
+  ROOM_FULL: 'room-full',
+  ROOM_NOT_FOUND: 'room-not-found',
+};
